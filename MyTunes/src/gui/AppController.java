1323d6c4eece5b5fb97f79c724df781207222455
René Jørgensen <56912824+LeChampDK@users.noreply.github.com>
2019-12-02 13:03:06 +0100

    Update + relative path

--- MyTunes/src/gui/AppController.java
+++ MyTunes/src/gui/AppController.java
@@ -7,16 +7,18 @@ package gui;
  */
 import be.Songs;
 import gui.model.SongModel;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -27,7 +29,6 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -37,11 +38,10 @@ import javafx.stage.Stage;
  *
  * @author Jacob
  */
-public class AppController implements Initializable
-{
+public class AppController implements Initializable {
 
     @FXML
-    private TextField search;
+    private TextField txtSearch;
     @FXML
     private ImageView reversebutton;
     @FXML
@@ -88,14 +88,59 @@ public class AppController implements Initializable
     private TableColumn<Songs, String> category;
     @FXML
     private TableColumn<Songs, Double> time;
-    
+
     private SongModel songModel;
     private MediaPlayer mediaPlay;
-    
-        
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        songName.setCellValueFactory(new PropertyValueFactory<Songs, String>("songName"));
+        songArtist.setCellValueFactory(new PropertyValueFactory<Songs, String>("songArtist"));
+        category.setCellValueFactory(new PropertyValueFactory<Songs, String>("category"));
+        time.setCellValueFactory(new PropertyValueFactory<Songs, Double>("time"));
+        try {
+            songModel = new SongModel();
+            allSongs.setItems(songModel.getAllSongs());
+        } catch (Exception ex) {
+            Logger.getLogger(AppController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        FilteredList<Songs> filteredData = new FilteredList<>(songModel.getAllSongs(), p -> true);
+
+        // 2. Set the filter Predicate whenever the filter changes.
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(Songs -> {
+                // If filter text is empty, display all persons.
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+
+                // Compare first name and last name of every person with filter text.
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (songName.getCellData(Songs).toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches first name.
+                } else if (songArtist.getCellData(Songs).toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches last name.
+                } else if (category.getCellData(Songs).toLowerCase().contains(lowerCaseFilter)) {
+                    return true; // Filter matches last name.
+                }
+                return false; // Does not match.
+            });
+        });
+
+        // 3. Wrap the FilteredList in a SortedList. 
+        SortedList<Songs> sortedData = new SortedList<>(filteredData);
+
+        // 4. Bind the SortedList comparator to the TableView comparator.
+        sortedData.comparatorProperty().bind(allSongs.comparatorProperty());
+
+        // 5. Add sorted (and filtered) data to the table.
+        allSongs.setItems(sortedData);
+    }
+
     @FXML
-    public void songsEditButton(ActionEvent event) throws IOException
-    {
+    public void songsEditButton(ActionEvent event) throws IOException {
         Parent loader = FXMLLoader.load(getClass().getResource("SongsEdit.fxml"));
 
         Scene scene = new Scene(loader);
@@ -105,56 +150,35 @@ public class AppController implements Initializable
         stage.setScene(scene);
         stage.show();
     }
-    
+
     @FXML
-    public void songsNewButton(ActionEvent event) throws IOException
-    {
+    public void songsNewButton(ActionEvent event) throws IOException {
         Parent loader = FXMLLoader.load(getClass().getResource("SongsNew.fxml"));
 
         Scene scene = new Scene(loader);
-        
+
         Stage stage = new Stage();
 
         stage.setScene(scene);
         stage.show();
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
-        songName.setCellValueFactory(new PropertyValueFactory<Songs, String>("songName"));
-        songArtist.setCellValueFactory(new PropertyValueFactory<Songs, String>("songArtist"));
-        category.setCellValueFactory(new PropertyValueFactory<Songs, String>("category"));
-        time.setCellValueFactory(new PropertyValueFactory<Songs, Double>("time"));
-        try
-        {
-            songModel = new SongModel();
-            allSongs.setItems(songModel.getAllSongs());
-        } catch (Exception ex)
-        {
-            Logger.getLogger(AppController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 
     @FXML
-    public void mousePressedPlay()
-    {
-        String bip = "file:/C:/Users/Jacob/Documents/GitHub/MyTunes/MyTunes/Music/Song1.mp3";
+    public void mousePressedPlay() {
+        String bip = new File("Music/Song1.mp3").toURI().toString();
         Media hit = new Media(bip);
         mediaPlay = new MediaPlayer(hit);
         mediaPlay.play();
     }
 
     @FXML
-    public void mousePressedNext()
-    {
-        
+    public void mousePressedNext() {
+
     }
 
     @FXML
-    public void mousePressedPrevious()
-    {
-        
+    public void mousePressedPrevious() {
+
     }
-    
+
 }
