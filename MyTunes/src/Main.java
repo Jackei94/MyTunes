4e9d3539bf6945d3f289e3163f9edff2e18d78a5
Trammern <55829858+Trammern@users.noreply.github.com>
2019-12-09 21:11:31 +0100

    Boom Baby

--- MyTunes/src/Main.java
+++ MyTunes/src/Main.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 /**
  *
@@ -18,7 +19,7 @@ public class Main extends Application
 {
     
     @Override
-    public void start(Stage stage) throws Exception
+    public void start(Stage stage) throws IOException
     {
         Parent root = FXMLLoader.load(getClass().getResource("gui/App.fxml"));
         
